72762218fcf8398505f157fdc18d7a2d6fa8f6bd
BCaven <blakecaven@outlook.com>
2024-02-04 21:26:03 -0500

    started working on getting audio from loopback devices

--- testing/get_audio_sources.py
+++ testing/get_audio_sources.py
@@ -11,8 +11,6 @@ for i in range(p.get_host_api_count()):
     device = p.get_host_api_info_by_index(i)
     print(f"{device['index']}: {device['name']}")
 
-print("audio inputs:")
-
 
 print("audio devices:")
 for i in range(p.get_device_count()):
@@ -24,3 +22,10 @@ print(p.get_default_input_device_info())
 
 print("default output:")
 print(p.get_default_output_device_info())
+
+print("loopbacks")
+for i in range(p.get_device_count()):
+    dev = p.get_device_info_by_index(i)
+    if (dev['hostApi'] == 0):
+        dev_index = dev['index']
+        print('dev_index: ', dev_index, " name: ", dev['name'])
