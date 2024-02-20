dbf8cb7ceae486da703c228199e80b11bbd5edac
Blake <blakecaven@outlook.com>
2024-02-20 17:48:40 -0500

    added switching inputs

--- testing/soundcard_testing.py
+++ testing/soundcard_testing.py
@@ -2,73 +2,67 @@ import soundcard as sc
 import numpy as np
 import sys
 import requests
-from time import time_ns, time
 
+# BUG: fuzzy search grabs loopback devices when given the name of the actual device (non-loopback)
 
-DOCKER_IP="http://192.168.86.34:8000/audio_in"
+DOCKER_IP="http://0.0.0.0:8000/"
+LOOPBACK = False
 
-RECORD = True
+blocksize = 1024
+samplerate = 48000
 
-speakers = sc.all_speakers()
-mics = sc.all_microphones(include_loopback=True)
+mics = sc.all_microphones(include_loopback=LOOPBACK)
+new_mic = sc.default_microphone().name
 
-print("all devices:")
-print("\n".join(f"{i.id} {i.name}" for i in speakers))
-print("\n".join(f"{i.id} {i.name}" for i in mics))
+audio_devices = {
+    "mics": [m.name for m in mics],
+    "source": new_mic
+}
+# send the list
+# for now we do not care about the response
+_ = requests.post(DOCKER_IP + "audio_source", data=audio_devices)
 
-default_mic = sc.default_microphone()
-default_speaker = sc.default_speaker()
-# alsa_output.usb-Corsair_CORSAIR_VIRTUOSO_XT_Wireless_Gaming_Receiver_16cad14300030215-00.pro-output-0.monitor
-# alsa_output.pci-0000_04_00.6.analog-stereo.monitor
-chosen_mic = sc.get_microphone("alsa_output.pci-0000_04_00.6.analog-stereo.monitor", include_loopback=True)
+while True:
+    try:
+        chosen_mic = sc.get_microphone(new_mic, include_loopback=LOOPBACK)
+        new_mic = chosen_mic.name
+        with chosen_mic.recorder(samplerate=samplerate, blocksize=blocksize) as mic:
+            current_name = chosen_mic.name
+            print(f"starting with: {current_name}")
+            print(f"new mic name: {new_mic}")
+            while current_name == new_mic:
+                # the actual number of frames in each chunk is the block size
+                # higher blocksize = easier on the system
+                # higher blocksizes inherently have latency because they have to 
+                # record the data before sending it
+                # but smaller block sizes eventually have infinite latency because
+                # the system cannot process the audio fast enough
+                # TODO: write script to auto adjust the blocksize
+                # TODO: section in Vue "options" for the user to change the blocksize
+                data = np.abs(mic.record(numframes=None))
+                avg = np.average(np.abs(data))
+                peak = np.max(np.abs(data))
 
-print(f"defaults:\nmic: {default_mic}\nspeaker: {default_speaker}")
-print(f"chosen mic: {chosen_mic}")
-
-# look at this:
-# https://soundcard.readthedocs.io/en/latest/#latency
-# with default settings latency is ~2 seconds
-# following the recomendations in the documentation, latency got down to ~0.2 seconds on one laptop
-# laptop with different audio setup started with low latency and the latency quickly climbed
-# changing the blocksize to 512 instead of 256 reduced the latency to a consistent 0.011 seconds
-
-if not RECORD:
-    sys.exit(0)
-
-with chosen_mic.recorder(samplerate=48000, blocksize=512) as mic:
-    while True:
-        try:
-            # numframes is how many samples are part of each chunk
-            # higher values mean larger (less sensitive) chunks
-            # however, higher frames are also easier on us because it means
-            # fewer post requests
-            # setting it to None returns the audio as soon as we get it
-            # the actual number of frames in each chunk is the block size
-            record_time = time()
-            data = np.abs(mic.record(numframes=None))
-            avg = np.average(np.abs(data))
-            peak = np.max(np.abs(data))
-
-            #bars = "#" * int(avg * 50)
-            #max_bars = "-" * int((50 * peak) - (50 * avg))
-            #print(data)
-            #print(f"{len(data)} {bars + max_bars}")
-            payload = {
-                "avg": avg,
-                "peak": peak,
-                "post time": time_ns()
-            }
-            send_time =  time() - record_time
-            response = requests.post(DOCKER_IP, data=payload).text
-            receive_time = time() - record_time
-            #print(f"{receive_time:4.3f} {send_time:4.3f} {response}")
-            #rpeak = float(response['peak'])
-            #ravg = float(response['avg'])
-            #bars = "#" * int(ravg * 50)
-            #max_bars = "-" * int((50 * rpeak) - (50 * ravg))
-
-            print(f"latency: {mic.latency:4.3f} {response}")
-        except KeyboardInterrupt:
-            break
-        # only works on linux
+                # TODO: experiment with sending raw data
+                # if you do, just add a new key called "data" with the raw np array
+                payload = {
+                    "avg": avg,
+                    "peak": peak,
+                    "data": data,
+                    "source": current_name
+                }
+                response = requests.post(DOCKER_IP + "audio_in", data=payload).json()
+                #print(response)
+                if "source" in response:
+                    # oh no we have to change audio devices
+                    new_mic = response["source"]
+                    print("switching mics")
+                #bars = "#" * int(50 * avg)
+                #mbars = "-" * int((50 * peak) - (50 * avg))
+                #print("local audio: " + bars + mbars)
+                # latency only works on linux
+                print(f"latency: {mic.latency:4.3f} mic: {current_name[:6]} {response['bars']}")
         
+    except KeyboardInterrupt:
+        print("exiting...")
+        sys.exit(0)
